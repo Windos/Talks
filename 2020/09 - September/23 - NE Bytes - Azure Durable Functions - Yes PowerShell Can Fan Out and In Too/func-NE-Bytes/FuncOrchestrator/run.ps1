81c242aa807ec466722d099be62e1d93f002567d
Josh (Windos) King <windosbk+github@gmail.com>
2020-09-22 21:19:08 +1200

    CompleteDemo

---
+++ 2020/09 - September/23 - NE Bytes - Azure Durable Functions - Yes PowerShell Can Fan Out and In Too/func-NE-Bytes/FuncOrchestrator/run.ps1
@@ -0,0 +1,20 @@
+param($Context)
+
+$Header = @{'X-API-Key' = 'ddd01090'}
+$Uri = 'https://my.api.mockaroo.com/demodata.json'
+
+$UserData = Invoke-RestMethod $Uri -Headers $Header | ConvertFrom-Csv
+
+Write-Warning "Start: $([datetime]::Now)"
+
+$Tasks = foreach ($User in $UserData) {
+    Invoke-ActivityFunction -FunctionName 'FuncActivity' -Input $User -NoWait
+}
+
+$output = Wait-ActivityFunction -Task $Tasks
+
+$TotalCount = $output.Result.Count
+$SuccessCount = ($output | Where-Object {$_.Result -eq $true}).Count
+$PctSuccess = ($SuccessCount / $TotalCount).ToString('P')
+
+'Employees online: {0}' -f $PctSuccess
